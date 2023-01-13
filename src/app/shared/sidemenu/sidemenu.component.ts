b069b9dca41921de95a5dffd1be18b23de9b6701
Jonathan San Miguel <guerreroz80@outlook.es>
2023-01-13 11:29:59 -0600

    creacion del arbol de archivos listo

---
+++ src/app/shared/sidemenu/sidemenu.component.ts
@@ -0,0 +1,30 @@
+import { Component } from '@angular/core'
+
+interface MenuItem{
+  texto: string
+  ruta: string
+}
+
+@Component({
+  selector: 'app-sidemenu',
+  templateUrl: './sidemenu.component.html',
+  styleUrls: ['./sidemenu.component.css']
+})
+export class SidemenuComponent {
+
+  templateMenu: MenuItem[] = [
+    {
+      texto: 'Basicos',
+      ruta: './template/basicos'
+    },
+    {
+      texto: 'Dinamicos',
+      ruta: './template/dinamicos'
+    },
+    {
+      texto: 'Switches',
+      ruta: './template/switches'
+    }
+  ]
+
+}
